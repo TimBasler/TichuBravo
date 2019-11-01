1db4b092daa38709ae1c72e75ecd53cfef08d0ba
TimBasler <47785013+TimBasler@users.noreply.github.com>
2019-11-01 14:29:31 +0100

    small fix

--- TichuBravo/src/client/ClientController.java
+++ TichuBravo/src/client/ClientController.java
@@ -1,14 +1,20 @@
 package client;
 
+import java.net.Socket;
+
+import org.json.simple.JSONObject;
+
+import server.ServerClient;
+
 //This Code is written by Tim
 
 public class ClientController {
 	private ClientModel clientModel;
 	private ClientView clientView;
-	
-	private int isClicked=0;
-	private int isPlayerOneCounter=0;
-	private int isPlayerTwoCounter=0;
+
+	private int isClicked = 0;
+	private int isPlayerOneCounter = 0;
+	private int isPlayerTwoCounter = 0;
 
 	public ClientController(ClientModel clientModel, ClientView clientView) {
 		this.clientModel = clientModel;
@@ -16,44 +22,44 @@ public class ClientController {
 
 		Player player = new Player();
 
-			clientView.lobbyView.loginButton.setOnAction(e -> {
-				this.isClicked++;
-				clientView.lobbyView.playerDefaultNumber++;
-				
-				
-				if(this.isClicked<5) {
-					if(this.isPlayerOneCounter>0) {
-						this.clientView.lobbyView.teamOne.setDisable(true);
-					}else if(this.isPlayerTwoCounter>0) {
-						this.clientView.lobbyView.teamTwo.setDisable(true);
-					}
-					String playerName = clientView.lobbyView.userTextField.getText();
-					boolean isTeamOne;
-					if (clientView.lobbyView.teamOne.isSelected()) {
-						isTeamOne = true;
-						this.isPlayerOneCounter++;
-					} else {
-						isTeamOne = false;
-						this.isPlayerTwoCounter++;
-					}
+		clientView.lobbyView.loginButton.setOnAction(e -> {
 
-					int playerID = 0;
-					player.increasePlayerID();
-					player.setPlayerID(player.getPlayerID());
+			this.isClicked++;
+			clientView.lobbyView.playerDefaultNumber++;
 
-					player.setPlayerName(playerName);
-					player.setIsTeamOne(isTeamOne);
-					clientView.stage.setScene(clientView.gameScene);
-
-					// TODO Delete This
-					System.out.println(player);
-				}else {
-					clientView.lobbyView.userTextField.setDisable(true);
-					clientView.lobbyView.teamOne.setDisable(true);
-					clientView.lobbyView.teamOne.setDisable(true);
+			if (this.isClicked < 5) {
+				if (this.isPlayerOneCounter > 0) {
+					this.clientView.lobbyView.teamOne.setDisable(true);
+				} else if (this.isPlayerTwoCounter > 0) {
+					this.clientView.lobbyView.teamTwo.setDisable(true);
 				}
-			});
-		
+				String playerName = clientView.lobbyView.userTextField.getText();
+				boolean isTeamOne;
+				if (clientView.lobbyView.teamOne.isSelected()) {
+					isTeamOne = true;
+					this.isPlayerOneCounter++;
+				} else {
+					isTeamOne = false;
+					this.isPlayerTwoCounter++;
+				}
+
+				int playerID = 0;
+				player.increasePlayerID();
+				player.setPlayerID(player.getPlayerID());
+
+				player.setPlayerName(playerName);
+				player.setIsTeamOne(isTeamOne);
+				clientView.stage.setScene(clientView.gameScene);
+
+				// TODO Delete This
+				System.out.println(player);
+			} else {
+				clientView.lobbyView.userTextField.setDisable(true);
+				clientView.lobbyView.teamOne.setDisable(true);
+				clientView.lobbyView.teamOne.setDisable(true);
+			}
+		});
+
 		clientView.gameView.button.setOnAction(e -> {
 			clientView.stage.setScene(clientView.lobbyScene);
 		});
