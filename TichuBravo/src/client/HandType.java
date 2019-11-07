e04f26e462d5e25bff6439d6aaa698a9aed49086
Dominik <dominik.meier3@students.fhnw.ch>
2019-11-07 13:27:57 +0000

    HandType method

--- TichuBravo/src/client/HandType.java
+++ TichuBravo/src/client/HandType.java
@@ -9,39 +9,29 @@ import common.Card.Suit;
 public enum HandType {
 	HighCard, OnePair, Pairs, ThreeOfAKind, FullHouse, Straight, BombFourOfAKind, BombStraightFlush;
 
-	// 1.Array = HighCard, 2.Array = OnePair, 3.Array = Pairs, etc...
-	public static ArrayList<ArrayList<Card>> handTypeList = new ArrayList<ArrayList<Card>>(8);
-	// 1.Array = 2Pairs in a row, 2.Array = 3Pairs in a row, etc...
-	public static ArrayList<ArrayList<Card>> pairsInARow = new ArrayList<ArrayList<Card>>(6);
+	// each pair is in an arrayList
+	public static ArrayList<ArrayList<Card>> onePairList = new ArrayList<ArrayList<Card>>();
+
+	// each pairs in a row is in an arrayList
+	public static ArrayList<ArrayList<Card>> pairsInARow = new ArrayList<ArrayList<Card>>();
+
+	// each ThreeOfAKind is in an arrayList
+	public static ArrayList<ArrayList<Card>> threeOfAKindList = new ArrayList<ArrayList<Card>>();
+
+	// each FullHouse is in an arrayList
+	public static ArrayList<ArrayList<Card>> fullHouseList = new ArrayList<ArrayList<Card>>();
+
 	// each straight is in an arrayList
 	public static ArrayList<ArrayList<Card>> straightList = new ArrayList<ArrayList<Card>>();
 
+	// each BombFourOfAKind is in an arrayList
 	public static ArrayList<ArrayList<Card>> bombList = new ArrayList<ArrayList<Card>>();
 
-//	Dog, Mah Jong, 2,3,4,5,6,7,8,9,10,J,Q,K,A,Phenix,Dragon
+	// each BombStraightFlush is in an arrayList
+	public static ArrayList<ArrayList<Card>> BombStraightFlushList = new ArrayList<ArrayList<Card>>();
 
-	public static HandType evaluateHand(ArrayList<Card> cards) {
-		HandType currentEval = HighCard;
-
-		if (isOnePair(cards))
-			currentEval = OnePair;
-		if (isTwoPair(cards))
-			currentEval = TwoPair;
-		if (isThreeOfAKind(cards))
-			currentEval = ThreeOfAKind;
-		if (isStraight(cards))
-			currentEval = Straight;
-		if (isFlush(cards))
-			currentEval = Flush;
-		if (isFullHouse(cards))
-			currentEval = FullHouse;
-		if (isFourOfAKind(cards))
-			currentEval = FourOfAKind;
-		if (isStraightFlush(cards))
-			currentEval = StraightFlush;
-
-		return currentEval;
-	}
+	// normal cards order: 2,3,4,5,6,7,8,9,10,J,Q,K,A
+	// special cards order: Dog, MahJong, Phenix, Dragon
 
 	public static boolean hasPhenix(ArrayList<Card> specialCards) {
 		boolean found = false;
@@ -83,18 +73,25 @@ public enum HandType {
 		return found;
 	}
 
+	/**
+	 * search for pairs and add it to onePairList
+	 * 
+	 * @param cards
+	 */
 	public static void findPair(ArrayList<Card> cards) {
 		ArrayList<Card> clonedCards = (ArrayList<Card>) cards.clone();
 
 		for (int i = 0; i < clonedCards.size() - 1; i++) {
 			for (int j = i + 1; j < clonedCards.size(); j++) {
 				if (clonedCards.get(i).getRank() == clonedCards.get(j).getRank()) {
-					handTypeList.get(1).add(clonedCards.get(i));
-					handTypeList.get(1).add(clonedCards.get(j));
+					ArrayList<Card> newList = new ArrayList<Card>();
+					newList.add(clonedCards.get(i));
+					newList.add(clonedCards.get(j));
 					clonedCards.remove(i);
 					clonedCards.remove(j + 1);
 					i = 0;
 					j = 0;
+					onePairList.add(newList);
 				}
 			}
 		}
@@ -108,7 +105,13 @@ public enum HandType {
 	 * if 2 pairs in a row, safe it in "pairsInARow" on first place (index 0)
 	 */
 	public static void findPairsInARow() {
-		ArrayList<Card> c = (ArrayList<Card>) handTypeList.get(1).clone();
+		ArrayList<Card> c = new ArrayList<Card>();
+
+		for (int i = 0; i < onePairList.size(); i++) {
+			for (int j = 0; j < onePairList.get(i).size(); j++) {
+				c.add(onePairList.get(i).get(j));
+			}
+		}
 
 		// if 7 pairs in a row, safe it in "pairsInARow" on sixth place
 		if (c.get(12) != null && c.get(0).getRank().ordinal() == c.get(2).getRank().ordinal() + 1
@@ -117,9 +120,11 @@ public enum HandType {
 				&& c.get(6).getRank().ordinal() == c.get(8).getRank().ordinal() + 1
 				&& c.get(8).getRank().ordinal() == c.get(10).getRank().ordinal() + 1
 				&& c.get(10).getRank().ordinal() == c.get(12).getRank().ordinal() + 1) {
+			ArrayList<Card> newList = new ArrayList<Card>();
 			for (int i = 0; i < 14; i++) {
-				pairsInARow.get(5).add(c.get(i));
+				newList.add(c.get(i));
 			}
+			pairsInARow.add(newList);
 		}
 
 		// if 6 pairs in a row, safe it in "pairsInARow" on fifth place
@@ -129,8 +134,9 @@ public enum HandType {
 					&& c.get(i + 4).getRank().ordinal() == c.get(i + 6).getRank().ordinal() + 1
 					&& c.get(i + 6).getRank().ordinal() == c.get(i + 8).getRank().ordinal() + 1
 					&& c.get(i + 8).getRank().ordinal() == c.get(i + 10).getRank().ordinal() + 1) {
+				ArrayList<Card> newList = new ArrayList<Card>();
 				for (int j = i; j < i + 12; j++) {
-					pairsInARow.get(4).add(c.get(j));
+					newList.add(c.get(j));
 				}
 			}
 		}
@@ -141,9 +147,11 @@ public enum HandType {
 					&& c.get(i + 2).getRank().ordinal() == c.get(i + 4).getRank().ordinal() + 1
 					&& c.get(i + 4).getRank().ordinal() == c.get(i + 6).getRank().ordinal() + 1
 					&& c.get(i + 6).getRank().ordinal() == c.get(i + 8).getRank().ordinal() + 1) {
+				ArrayList<Card> newList = new ArrayList<Card>();
 				for (int j = i; j < i + 10; j++) {
-					pairsInARow.get(3).add(c.get(j));
+					newList.add(c.get(j));
 				}
+				pairsInARow.add(newList);
 			}
 		}
 
@@ -152,9 +160,11 @@ public enum HandType {
 			if (c.get(i + 6) != null && c.get(i).getRank().ordinal() == c.get(i + 2).getRank().ordinal() + 1
 					&& c.get(i + 2).getRank().ordinal() == c.get(i + 4).getRank().ordinal() + 1
 					&& c.get(i + 4).getRank().ordinal() == c.get(i + 6).getRank().ordinal() + 1) {
+				ArrayList<Card> newList = new ArrayList<Card>();
 				for (int j = i; j < i + 8; j++) {
-					pairsInARow.get(2).add(c.get(j));
+					newList.add(c.get(j));
 				}
+				pairsInARow.add(newList);
 			}
 		}
 
@@ -162,59 +172,31 @@ public enum HandType {
 		for (int i = 0; i < 9; i = i + 2) {
 			if (c.get(i + 4) != null && c.get(i).getRank().ordinal() == c.get(i + 2).getRank().ordinal() + 1
 					&& c.get(i + 2).getRank().ordinal() == c.get(i + 4).getRank().ordinal() + 1) {
+				ArrayList<Card> newList = new ArrayList<Card>();
 				for (int j = i; j < i + 6; j++) {
-					pairsInARow.get(1).add(c.get(j));
+					newList.add(c.get(j));
 				}
+				pairsInARow.add(newList);
 			}
 		}
 
 		// if 2 pairs in a row, safe it in "pairsInARow" on first place
 		for (int i = 0; i < 11; i = i + 2) {
 			if (c.get(i + 2) != null && c.get(i).getRank().ordinal() == c.get(i + 2).getRank().ordinal() + 1) {
+				ArrayList<Card> newList = new ArrayList<Card>();
 				for (int j = i; j < i + 4; j++) {
-					pairsInARow.get(0).add(c.get(j));
+					newList.add(c.get(j));
 				}
+				pairsInARow.add(newList);
 			}
 		}
 
 	} // end of method pairs
 
-	/**
-	 * // TwoPair private static int ordinal;
-	 * 
-	 * public static boolean isTwoPair(ArrayList<Card> cards) { // Clone the cards,
-	 * because we will be altering the list ArrayList<Card> clonedCards =
-	 * (ArrayList<Card>) cards.clone();
-	 * 
-	 * // Find the first pair; if found, remove the cards from the list boolean
-	 * firstPairFound = false; for (int i = 0; i < clonedCards.size() - 1 &&
-	 * !firstPairFound; i++) { for (int j = i + 1; j < clonedCards.size() &&
-	 * !firstPairFound; j++) { if (clonedCards.get(i).getRank() ==
-	 * clonedCards.get(j).getRank()) { ordinal =
-	 * clonedCards.get(i).getRank().ordinal(); firstPairFound = true;
-	 * clonedCards.remove(j); // Remove the later card clonedCards.remove(i); //
-	 * Before the earlier one } } } boolean secondPairIsOneBiggerThanFirstPair =
-	 * false; boolean secondPairIsOneSmalerThanFirstPair = false;
-	 * 
-	 * boolean foundSecond = false; for (int i = 0; i < clonedCards.size() - 1 &&
-	 * !foundSecond; i++) { for (int j = i + 1; j < clonedCards.size() &&
-	 * !foundSecond; j++) {
-	 * 
-	 * if (clonedCards.get(i).getRank() == clonedCards.get(j).getRank()) foundSecond
-	 * = true; int ordinalSecond = clonedCards.get(i).getRank().ordinal();
-	 * ordinal++; if (ordinal == ordinalSecond) { secondPairIsOneSmalerThanFirstPair
-	 * = true; secondPairIsOneBiggerThanFirstPair = true; } else { foundSecond =
-	 * false; } ordinal--; ordinal--; if (ordinal == ordinalSecond) {
-	 * secondPairIsOneBiggerThanFirstPair = true; secondPairIsOneSmalerThanFirstPair
-	 * = true; } else { foundSecond = false; } }
-	 * 
-	 * } return foundSecond; }
-	 */
+	
 
 	/**
-	 * search for three of a kind and add it to handTypeList on fourth place (index
-	 * 3)
-	 * 
+	 * search for three of a kind and add it to threeOfAKindList
 	 * @param cards
 	 */
 	public static void findThreeOfAKind(ArrayList<Card> cards) {
@@ -225,32 +207,29 @@ public enum HandType {
 				for (int k = j + 1; k < clonedCards.size(); k++) {
 					if (clonedCards.get(i).getRank() == clonedCards.get(j).getRank()
 							&& clonedCards.get(j).getRank() == clonedCards.get(k).getRank()) {
-						handTypeList.get(3).add(clonedCards.get(i));
-						handTypeList.get(3).add(clonedCards.get(j));
-						handTypeList.get(3).add(clonedCards.get(k));
+						ArrayList<Card> newList = new ArrayList<Card>();
+						newList.add(clonedCards.get(i));
+						newList.add(clonedCards.get(j));
+						newList.add(clonedCards.get(k));
 						clonedCards.remove(i);
 						clonedCards.remove(j + 1);
 						clonedCards.remove(k + 2);
 						i = 0;
 						j = 0;
 						k = 0;
+						threeOfAKindList.add(newList);
 					}
 				}
 			}
 		}
 	}
 
-	/**
-	 * public static boolean isThreeOfAKind(ArrayList<Card> cards) { boolean found =
-	 * false; for (int i = 0; i < cards.size() - 2 && !found; i++) { for (int j = i
-	 * + 1; j < cards.size() - 1 && !found; j++) { for (int k = j + 1; k <
-	 * cards.size() && !found; k++) {
-	 * 
-	 * if ((cards.get(i).getRank() == cards.get(j).getRank()) &&
-	 * (cards.get(i).getRank() == cards.get(k).getRank())) found = true; } } }
-	 * return found; }
-	 */
 
+
+	/**
+	 * search for five or more cards in a row and add it to straightList
+	 * @param cards
+	 */
 	public static void findStraight(ArrayList<Card> cards) {
 		ArrayList<Card> sortList = (ArrayList<Card>) cards.clone();
 		Collections.sort(sortList);
@@ -440,88 +419,38 @@ public enum HandType {
 	}
 
 	/**
-	 * public static boolean isStraight(ArrayList<Card> cards) { ArrayList<Card>
-	 * sortList = cards; Collections.sort(sortList); // 5 in a row if
-	 * (sortList.get(0).getRank().ordinal() + 1 ==
-	 * sortList.get(1).getRank().ordinal() && sortList.get(1).getRank().ordinal() +
-	 * 1 == sortList.get(2).getRank().ordinal() &&
-	 * sortList.get(2).getRank().ordinal() + 1 ==
-	 * sortList.get(3).getRank().ordinal() && sortList.get(3).getRank().ordinal() +
-	 * 1 == sortList.get(4).getRank().ordinal()) { return true; } else { // Ace as
-	 * first card Ace, 2, 3, 4, 5 if (sortList.get(0).getRank().ordinal() == 0 &&
-	 * sortList.get(1).getRank().ordinal() == 1 &&
-	 * sortList.get(2).getRank().ordinal() == 2 &&
-	 * sortList.get(3).getRank().ordinal() == 3 &&
-	 * sortList.get(4).getRank().ordinal() == 12) { return true; } else return
-	 * false; } }
+	 * if the threeOfAKind and onePair are not the same cards, then add it to fullHouseList
 	 */
+	public static void findFullHouse() {
+		for (int i = 0; i < threeOfAKindList.size(); i++) {
+			for (int a = 0; a < onePairList.size(); a++) {
 
-	public static boolean isFlush(ArrayList<Card> cards) {
-		int num = 0;
-		for (int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).getSuit() == Suit.Jade) {
-				num++;
-				if (num == 5)
-					return true;
-			}
-		}
-		num = 0;
-		for (int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).getSuit() == Suit.Pagodas) {
-				num++;
-				if (num == 5)
-					return true;
-			}
-		}
-		num = 0;
-		for (int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).getSuit() == Suit.Stars) {
-				num++;
-				if (num == 5)
-					return true;
-			}
-		}
-		num = 0;
-		for (int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).getSuit() == Suit.Machete) {
-				num++;
-				if (num == 5)
-					return true;
-			}
-		}
-
-		return false;
-	}
-
-	public static boolean isFullHouse(ArrayList<Card> cards) {
-		// Clone the cards, because we will be altering the list
-		ArrayList<Card> clonedCards = (ArrayList<Card>) cards.clone();
-
-		// Find the first pair; if found, remove the cards from the list
-		boolean first = false;
-		boolean second = false;
-		// ThreeOfAKind search and remove
-		for (int i = 0; i < clonedCards.size() - 2 && !first; i++) {
-			for (int j = i + 1; j < clonedCards.size() - 1 && !first; j++) {
-				for (int k = j + 1; k < clonedCards.size() && !first; k++) {
-					if (clonedCards.get(i).getRank() == clonedCards.get(j).getRank()
-							&& clonedCards.get(j).getRank() == clonedCards.get(k).getRank()) {
-						first = true;
-						clonedCards.remove(k);
-						clonedCards.remove(j);
-						clonedCards.remove(i);
+				boolean different = true;
+				for (int j = 0; j < threeOfAKindList.get(i).size(); j++) {
+					for (int b = 0; b < onePairList.get(a).size(); b++) {
+						if (threeOfAKindList.get(i).get(j).isEqual(onePairList.get(a).get(b))) {
+							different = false;
+						}
 					}
+				}
+				if (different) {
+					ArrayList<Card> newList = new ArrayList<Card>();
+					for (int j = 0; j < threeOfAKindList.get(i).size(); j++) {
+						newList.add(threeOfAKindList.get(i).get(j));
+					}
+					for (int b = 0; b < onePairList.get(a).size(); b++) {
+						newList.add(onePairList.get(a).get(b));
+					}
+					fullHouseList.add(newList);
 				}
 			}
 		}
-		// the last two cards should be a pair
-		if (clonedCards.get(0).getRank().ordinal() == clonedCards.get(1).getRank().ordinal()) {
-			second = true;
-		}
-		// If a first threeOfAKInd was found, see if there is a second pair
-		return first && second;
 	}
 
+	/**
+	 * search four of a kind
+	 * @param cards
+	 */
 	public static void findBomb(ArrayList<Card> cards) {
 		ArrayList<Card> clonedCards = (ArrayList<Card>) cards.clone();
 
@@ -548,8 +477,24 @@ public enum HandType {
 
 	}
 
-	public static boolean isStraightFlush(ArrayList<Card> cards) {
-		return isFlush(cards) && isStraight(cards);
+	/**
+	 * if the straightList contains a Straight Flush, then add it to the BombStraightFlushList
+	 */
+	public static void findBombStraightFlush() {
+		for (int i = 0; i < straightList.size(); i++) {
+			int counter = 0;
+			for (int j = 0; j < straightList.get(i).size() - 1; j++) {
+				for (int k = j + 1; k < straightList.get(i).size(); k++) {
+					if (straightList.get(i).get(j).getSuit() == straightList.get(i).get(k).getSuit()) {
+						counter++;
+					}
+				}
+			}
+			if (counter + 1 == straightList.get(i).size()) {
+				ArrayList<Card> clonedCards = (ArrayList<Card>) straightList.get(i).clone();
+				BombStraightFlushList.add(clonedCards);
+			}
+		}
 	}
 
 }
